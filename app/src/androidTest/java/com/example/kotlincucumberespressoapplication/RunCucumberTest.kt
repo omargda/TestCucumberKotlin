b5bc90d4954f98629461905dd4e999e07ba33f0d
Omar Garcia de Alba <ogarciadealba@mv1m8md6v.corp.siriusxm.com>
2021-05-06 13:33:37 -0700

    Uploading test project for Cucumber for Kotlin.

---
+++ app/src/androidTest/java/com/example/kotlincucumberespressoapplication/RunCucumberTest.kt
@@ -0,0 +1,10 @@
+package com.example.kotlincucumberespressoapplication
+
+import io.cucumber.junit.Cucumber
+import io.cucumber.junit.CucumberOptions
+import org.junit.runner.RunWith
+
+@RunWith(Cucumber::class)
+@CucumberOptions()
+class RunCucumberTest {
+}
---
+++ app/src/androidTest/resources/com.example.kotlincucumberespressoapplication/mainFeature.feature
@@ -0,0 +1,4 @@
+Feature: Main
+  Scenario: User clicks button
+    Given User opened app
+    When User presses button
